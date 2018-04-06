a549ba8a61774aa432a3fcfeb22c84ac29cd9ac0
Lai Lee <lai_li@htc.com>
2018-04-06 22:03:41 +0800

    Update urls

--- app/src/main/java/com/lai/news/NewsUrlProvider.kt
+++ app/src/main/java/com/lai/news/NewsUrlProvider.kt
@@ -4,20 +4,35 @@ import java.net.URLEncoder
 import java.util.*
 
 class NewsUrlProvider {
-    var urls = arrayOf("https://api.myjson.com/bins/van6b",
-            "https://api.myjson.com/bins/kkuab",
-            "https://api.myjson.com/bins/12txdn",
+
+    var urls = arrayOf(
+            "https://api.myjson.com/bins/dl0p7",
+            "https://api.myjson.com/bins/1f3bt7",
+            "https://api.myjson.com/bins/1axakr",
+            "https://api.myjson.com/bins/j02az",
+            "https://api.myjson.com/bins/8vp0r",
             "https://api.myjson.com/bins/riovv"
             )
+
+    var detailUrls = arrayOf(
+            "https://api.myjson.com/bins/xy9az",
+            "https://api.myjson.com/bins/kezmz",
+            "https://api.myjson.com/bins/61117",
+            "https://api.myjson.com/bins/6mgmz",
+            "https://api.myjson.com/bins/v18fv",
+            "https://api.myjson.com/bins/jpzy3"
+    )
+
     companion object {
 
         fun genNewsDetailUrl(uuid: String): String {
-            return "https://api.myjson.com/bins/kezmz" //return only one news with body
+            var i = (0..NewsUrlProvider().detailUrls.size).random()
+            return NewsUrlProvider().detailUrls[i] //return only one news with body
         }
 
         fun generateUrl(keywords: String, page: Int): String {
             when (BuildConfig.DEBUG) {
-                true -> return NewsUrlProvider().urls[page]
+                true -> return NewsUrlProvider().urls[page - 1]
                 false -> return newsApi(keywords, page)
             }
         }
@@ -30,5 +45,8 @@ class NewsUrlProvider {
             newKeywords = URLEncoder.encode(newKeywords, "UTF-8")
             return BuildConfig.NEWSAPI_BASE_URL + "/v2/everything?q=" + newKeywords + "&sortBy=publishedAt&language=en&page=" + page + "&apiKey=" +  BuildConfig.NEWSAPI_API_KEY
         }
+
+        fun ClosedRange<Int>.random() =
+                Random().nextInt(endInclusive - start) +  start
     }
 }
